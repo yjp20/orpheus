9d6517fd06df83b5db614dc6fbc20d3fb48d9d30
Young-Jin Park <youngjinpark20@gmail.com>
2021-12-23 08:59:19 -0800

    Simplify downloading process to just wav
    Encoding to mp3 takes a significant amount of time, which takes away
    from the user experience. Ideally we wouldn't have to transcode at all,
    but wav is a good compromise for now.

--- cmd/orpheus/downloader.go
+++ cmd/orpheus/downloader.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"os"
 	"os/exec"
-	"strconv"
 	"strings"
+	"strconv"
 	"time"
-
-	mp3 "github.com/hajimehoshi/go-mp3"
 )
 
 func fetchSongsFromURL(url string, playlist bool) (songs []*Song, err error) {
@@ -17,83 +12,26 @@ func fetchSongsFromURL(url string, playlist bool) (songs []*Song, err error) {
 	if playlist {
 		listFlag = "--yes-playlist"
 	}
-	metaDataProcess := exec.Command("yt-dlp", listFlag, "--print", "%(title)s\n%(id)s\n%(duration)d", "--no-warnings", url)
+	metaDataProcess := exec.Command("yt-dlp", listFlag, "--id", "-q", "-f", "ba", "-x", "--audio-format", "wav", "--no-simulate", "--print", "%(title)s\n%(id)s\n%(duration)d\n%(acodec)s", "-P", "./data", "--no-warnings", url)
 	metaData, err := metaDataProcess.Output()
 	if err != nil {
 		return nil, err
 	}
+
 	tokens := strings.Split(string(metaData), "\n")
-	numSongs := len(tokens) / 3
-	songs = make([]*Song, 0)
+	numSongs := len(tokens) / 4
+	songs = make([]*Song, numSongs)
 	for i := 0; i < numSongs; i++ {
-		song := &Song{
-			Name:         tokens[i*3],
-			ID:           tokens[i*3+1],
-			File:         "./data/" + tokens[i*3+1] + ".mp3",
-			IsDownloaded: true,
-			download:     make(chan int),
+		title, id, duration, _ := tokens[4*i], tokens[4*i+1], tokens[4*i+2], tokens[4*i+3]
+		seconds, _:= strconv.Atoi(duration)
+		songs[i] = &Song{
+			Name:   title,
+			ID:     id,
+			File:   "./data/" + id  + ".wav",
+			Length: time.Duration(seconds) * time.Second,
+			Format: Wav,
 		}
-
-		if _, err := os.Stat(song.File); errors.Is(err, os.ErrNotExist) {
-			song.IsDownloaded = false
-			go downloadSong(song.ID, song)
-		}
-
-		if song.IsDownloaded {
-			_, song.Length, err = getMP3MetaData(song.File)
-			if err != nil {
-				return nil, err
-			}
-		} else {
-			seconds, err := strconv.Atoi(tokens[i*3+2])
-			song.Length = time.Second * time.Duration(seconds)
-			if err != nil {
-				return nil, err
-			}
-		}
-		songs = append(songs, song)
 	}
 
 	return songs, nil
-}
-
-func downloadSong(url string, song *Song) error {
-	downloadProcess := exec.Command("yt-dlp", "--id", "-x", "--audio-format", "mp3", "-P", "./data", url)
-	err := downloadProcess.Run()
-	if err != nil {
-		return fmt.Errorf("Failed to download '%s'\nerror: %s\n", url, err.Error())
-	}
-	var sampleRate int
-	sampleRate, song.Length, err = getMP3MetaData(song.File)
-	if sampleRate != 48000 {
-		tempPath := "/tmp/" + song.ID + ".mp3"
-		resampleProcess := exec.Command("ffmpeg", "-i", song.File, "-ar", "48000", tempPath)
-		err = resampleProcess.Run()
-		if err != nil {
-			return err
-		}
-		err = os.Rename(tempPath, song.File)
-		if err != nil {
-			return err
-		}
-	}
-
-	song.IsDownloaded = true
-	song.download <- 0
-	close(song.download)
-	return nil
-}
-
-func getMP3MetaData(path string) (sampleRate int, length time.Duration, err error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return
-	}
-	d, err := mp3.NewDecoder(file)
-	if err != nil {
-		return
-	}
-	sampleRate = d.SampleRate()
-	length = time.Duration(d.Length() * int64(time.Second) / int64(sampleRate) / 4)
-	return
 }
--- go.mod
+++ go.mod
@@ -3,19 +3,16 @@ module github.com/yjp20/orpheus
 go 1.17
 
 require (
-	github.com/bwmarrin/dgvoice v0.0.0-20210225172318-caaac756e02e // indirect
-	github.com/bwmarrin/discordgo v0.23.3-0.20211010150959-f0b7e81468f7 // indirect
-	github.com/faiface/beep v1.1.0 // indirect
+	github.com/bwmarrin/discordgo v0.23.3-0.20211010150959-f0b7e81468f7
+	github.com/go-audio/audio v1.0.0
+	github.com/go-audio/wav v1.0.0
+	github.com/julienschmidt/httprouter v1.3.0
+	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32
+)
+
+require (
+	github.com/go-audio/riff v1.0.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/hajimehoshi/go-mp3 v0.3.0 // indirect
-	github.com/hajimehoshi/oto v1.0.1 // indirect
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/tcolgate/mp3 v0.0.0-20170426193717-e79c5a46d300 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
-	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
-	golang.org/x/mobile v0.0.0-20210924032853-1c027f395ef7 // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
-	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32 // indirect
 )
