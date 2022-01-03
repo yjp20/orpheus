0d9f8d00cecf42c40f613c38bd6093c85268ce9b
Young-Jin Park <youngjinpark20@gmail.com>
2022-01-03 15:30:26 -0800

    Refactor queue to be very slightly more idiomatic

--- pkg/queue/queue.go
+++ pkg/queue/queue.go
@@ -51,8 +51,8 @@ func NewQueue() Queue {
 }
 
 // Adds multiple songs to the queue based on policy.
-func (queue *Queue) Add(songs []*music.Song, userId string, shuffle bool, policy AddPolicy) []*QueueItem {
-	queue.mu.Lock()
+func (q *Queue) Add(songs []*music.Song, userId string, shuffle bool, policy AddPolicy) []*QueueItem {
+	q.mu.Lock()
 	items := make([]*QueueItem, len(songs))
 	for i, song := range songs {
 		items[i] = &QueueItem{song, userId}
@@ -61,163 +61,163 @@ func (queue *Queue) Add(songs []*music.Song, userId string, shuffle bool, policy
 		rand.Seed(time.Now().UnixNano())
 		rand.Shuffle(len(items), func(i, j int) { items[i], items[j] = items[j], items[i] })
 	}
-	list := make([]*QueueItem, len(queue.List)+len(items))
+	list := make([]*QueueItem, len(q.List)+len(items))
 	switch policy {
 	case Next, Now:
-		for i := 0; i <= queue.Index; i++ {
-			list[i] = queue.List[i]
+		for i := 0; i <= q.Index; i++ {
+			list[i] = q.List[i]
 		}
 		for i := 0; i < len(items); i++ {
-			list[queue.Index+1+i] = items[i]
+			list[q.Index+1+i] = items[i]
 		}
-		for i := queue.Index + 1; i < len(queue.List); i++ {
-			list[i+len(items)] = queue.List[i]
+		for i := q.Index + 1; i < len(q.List); i++ {
+			list[i+len(items)] = q.List[i]
 		}
-		queue.Dynamic = 0
+		q.Dynamic = 0
 	case Last:
-		for i := 0; i < len(queue.List); i++ {
-			list[i] = queue.List[i]
+		for i := 0; i < len(q.List); i++ {
+			list[i] = q.List[i]
 		}
 		for i := 0; i < len(items); i++ {
-			list[len(queue.List)+i] = items[i]
+			list[len(q.List)+i] = items[i]
 		}
-		queue.Dynamic = 0
+		q.Dynamic = 0
 	case Smart:
-		start := max(queue.Index, 0)
-		for i := start; i < queue.Dynamic; i++ {
-			if queue.List[i].QueuedBy == userId {
+		start := max(q.Index, 0)
+		for i := start; i < q.Dynamic; i++ {
+			if q.List[i].QueuedBy == userId {
 				start = i
 			}
 		}
 		for i := 0; i < start; i++ {
-			list[i] = queue.List[i]
+			list[i] = q.List[i]
 		}
 		count := map[string]int{}
 		for j, item := range items {
-			for start < queue.Dynamic && count[queue.List[start].QueuedBy] <= j {
-				list[start+j] = queue.List[start]
-				count[queue.List[start].QueuedBy]++
+			for start < q.Dynamic && count[q.List[start].QueuedBy] <= j {
+				list[start+j] = q.List[start]
+				count[q.List[start].QueuedBy]++
 				start++
 			}
 			list[start+j] = item
 		}
-		for i := start; i < len(queue.List); i++ {
-			list[i+len(items)] = queue.List[i]
+		for i := start; i < len(q.List); i++ {
+			list[i+len(items)] = q.List[i]
 		}
-		queue.Dynamic = max(queue.Index+1, queue.Dynamic) + len(items)
+		q.Dynamic = max(q.Index+1, q.Dynamic) + len(items)
 	}
-	queue.List = list
-	if policy == Now || queue.Index == -1 {
-		queue.Index = queue.Index + 1
-		queue.update()
+	q.List = list
+	if policy == Now || q.Index == -1 {
+		q.Index = q.Index + 1
+		q.update()
 	}
-	queue.mu.Unlock()
+	q.mu.Unlock()
 	return items
 }
 
-func (queue *Queue) SkipTo(index int) (*QueueItem, error) {
-	queue.mu.Lock()
-	if index >= len(queue.List) || index < 0 {
+func (q *Queue) SkipTo(index int) (*QueueItem, error) {
+	q.mu.Lock()
+	if index >= len(q.List) || index < 0 {
 		return nil, fmt.Errorf("index out of bounds")
 	}
-	queue.Dynamic = 0
-	queue.Index = index
-	queue.update()
-	queue.mu.Unlock()
-	return queue.List[index], nil
+	q.Dynamic = 0
+	q.Index = index
+	q.update()
+	q.mu.Unlock()
+	return q.List[index], nil
 }
 
-func (queue *Queue) Move(from, to int) (*QueueItem, error) {
-	queue.mu.Lock()
-	if from >= len(queue.List) || to >= len(queue.List) || from < 0 || to < 0 {
+func (q *Queue) Move(from, to int) (*QueueItem, error) {
+	q.mu.Lock()
+	if from >= len(q.List) || to >= len(q.List) || from < 0 || to < 0 {
 		return nil, fmt.Errorf("index out of bounds")
 	}
-	queue.Dynamic = 0
-	target := queue.List[from]
-	queue.List = append(queue.List[:from], queue.List[from+1:]...)
-	queue.mu.Unlock()
+	q.Dynamic = 0
+	target := q.List[from]
+	q.List = append(q.List[:from], q.List[from+1:]...)
+	q.mu.Unlock()
 	return target, nil
 }
 
-func (queue *Queue) Remove(index int) (*QueueItem, error) {
-	queue.mu.Lock()
-	if index >= len(queue.List) || index < 0 {
+func (q *Queue) Remove(index int) (*QueueItem, error) {
+	q.mu.Lock()
+	if index >= len(q.List) || index < 0 {
 		return nil, fmt.Errorf("index out of bounds")
 	}
-	target := queue.List[index]
-	queue.List = append(queue.List[:index], queue.List[index+1:]...)
-	if index == queue.Index {
-		queue.update()
-	} else if index < queue.Index {
-		queue.Index -= 1
+	target := q.List[index]
+	q.List = append(q.List[:index], q.List[index+1:]...)
+	if index == q.Index {
+		q.update()
+	} else if index < q.Index {
+		q.Index -= 1
 	}
-	queue.mu.Unlock()
+	q.mu.Unlock()
 	return target, nil
 }
 
 // Clears the queue
-func (queue *Queue) Clear() {
-	queue.List = []*QueueItem{}
-	queue.Index = -1
-	queue.update()
+func (q *Queue) Clear() {
+	q.List = []*QueueItem{}
+	q.Index = -1
+	q.update()
 }
 
 // Shuffles all items from the current index+1 to the end of the queue.
-func (queue *Queue) Shuffle() {
-	queue.mu.Lock()
-	if len(queue.List) == 0 {
+func (q *Queue) Shuffle() {
+	q.mu.Lock()
+	if len(q.List) == 0 {
 		return
 	}
-	offset := queue.Index + 1
+	offset := q.Index + 1
 	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(queue.List)-offset, func(i, j int) {
-		queue.List[i+offset], queue.List[j+offset] = queue.List[j+offset], queue.List[i+offset]
+	rand.Shuffle(len(q.List)-offset, func(i, j int) {
+		q.List[i+offset], q.List[j+offset] = q.List[j+offset], q.List[i+offset]
 	})
-	queue.mu.Unlock()
+	q.mu.Unlock()
 }
 
 // Loads the next item in the queue determined by policy defined by the
 // NextPolicy member of Queue.
-func (queue *Queue) NextSong() {
-	queue.mu.Lock()
-	if len(queue.List) == 0 {
+func (q *Queue) NextSong() {
+	q.mu.Lock()
+	if len(q.List) == 0 {
 		return
 	}
-	switch queue.NextPolicy {
+	switch q.NextPolicy {
 	case LoopSong:
 		break
 	case LoopQueue:
-		queue.Index = (queue.Index + 1) % len(queue.List)
+		q.Index = (q.Index + 1) % len(q.List)
 	case NoLoop:
-		queue.Index = queue.Index + 1
-		if queue.Index >= len(queue.List) {
-			queue.Index = -1
+		q.Index = q.Index + 1
+		if q.Index >= len(q.List) {
+			q.Index = -1
 		}
 	}
-	queue.update()
-	queue.mu.Unlock()
+	q.update()
+	q.mu.Unlock()
 }
 
 // Returns the current item in the queue that should be being played. Returns
 // nil if no such item exists.
-func (queue *Queue) CurrentItem() *QueueItem {
-	if queue.Index == -1 {
+func (q *Queue) CurrentItem() *QueueItem {
+	if q.Index == -1 {
 		return nil
 	}
-	return queue.List[queue.Index]
+	return q.List[q.Index]
 }
 
 // This is a utility function that calls the UpdateHandler based on the value
 // of the current Index in the queue
-func (queue *Queue) update() {
-	if queue.UpdateHandler == nil {
+func (q *Queue) update() {
+	if q.UpdateHandler == nil {
 		return
 	}
-	if queue.CurrentItem() == nil {
-		queue.UpdateHandler(nil)
+	if q.CurrentItem() == nil {
+		q.UpdateHandler(nil)
 		return
 	}
-	queue.UpdateHandler(queue.CurrentItem().Song)
+	q.UpdateHandler(q.CurrentItem().Song)
 }
 
 func max(x, y int) int {
